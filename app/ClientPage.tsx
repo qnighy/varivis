94a401384de60ccda025bcf0c455b805e0a76511
Masaki Hara <ackie.h.gmai@gmail.com>
2025-03-28 23:59:31 +0900

    Move existing code in the Next.js app

---
+++ app/ClientPage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ToastContainer } from "react-toastify";
+import { App } from "./App.js";
+
+export function ClientPage() {
+  return (<>
+    <App />
+    <ToastContainer />
+  </>);
+}
