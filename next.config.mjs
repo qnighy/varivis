325e5c0bc4f21b9a4d421c3385d858b44ee1a112
Masaki Hara <ackie.h.gmai@gmail.com>
2025-03-29 10:12:05 +0900

    Switch back to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+// Using *.mjs rather than *.ts
+// so that actions/configure-pages correctly works.
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+};
+
+export default nextConfig;
